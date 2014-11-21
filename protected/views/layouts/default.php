0510d606f6e3c014d1c3948c301624ca6701d263
Целищев Семен <tselishev.semen@yandex.ru>
2014-11-21 17:01:03 +0300

    M:  refactoring main template

--- protected/views/layouts/default.php
+++ protected/views/layouts/default.php
@@ -1,43 +1,41 @@
 <?
 /* @var $this SiteController */
-use \ClassModules\User\User;
-
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8"/>
-    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
-    <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
-    <link href="/css/libs/erp.css" rel="stylesheet">
-    <link href="/css/main.css" rel="stylesheet">
-    <? if (defined('CHOCOLATE_DEBUG')): ?>
-        <script src="/js/erp.js"></script>
-    <? else: ?>
-        <script src="/js/erp.min.js"></script>
-    <? endif; ?>
-    <script src="/js/main.js"></script>
-    <? echo CHtml::tag('title', [], Yii::app()->name) ?>
-</head>
-<body>
-<header id="header"></header>
-<div id="pagewrap">
-    <ul id="gn-menu" class="gn-menu-main"></ul>
-    <div id="content">
-        <? echo $content; ?>
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+        <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+        <link href="/css/libs/erp.css" rel="stylesheet">
+        <link href="/css/main.css" rel="stylesheet">
+        <? if (defined('CHOCOLATE_DEBUG')): ?>
+            <script src="/js/erp.js"></script>
+        <? else: ?>
+            <script src="/js/erp.min.js"></script>
+        <? endif; ?>
+        <script src="/js/main.js"></script>
+        <? echo CHtml::tag('title', [], Yii::app()->name) ?>
+    </head>
+    <body>
+    <header id="header"></header>
+    <div id="pagewrap">
+        <ul id="gn-menu" class="gn-menu-main"></ul>
+        <div id="content">
+            <? echo $content; ?>
+        </div>
     </div>
-</div>
-<footer id="footer">
-    <?
-    $userName = Yii::app()->user->fullName;
-    $userID = Yii::app()->user->id;
-    Yii::app()->clientScript->registerScript('start', <<<JS
-    facade.startApp('$userID', '$userName');
+    <footer id="footer">
+    </footer>
+    </body>
+    </html>
+<?
+$user = Yii::app()->user;
+$name = $user->fullName;
+$id = $user->id;
+Yii::app()->clientScript->registerScript('start', <<<JS
+    facade.startApp('$id', '$name');
 JS
-        , CClientScript::POS_READY
-    )
-    ?>
-
-</footer>
-</body>
-</html>
+    , CClientScript::POS_READY
+)
+?>
