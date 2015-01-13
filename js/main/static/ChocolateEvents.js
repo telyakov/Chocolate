fdc98de1025c7f8c377119187f580f234add39a4
Целищев Семен <tselishev.semen@yandex.ru>
2015-01-13 15:13:06 +0300

    B: fix for save button in tops

--- js/main/static/ChocolateEvents.js
+++ js/main/static/ChocolateEvents.js
@@ -695,8 +695,8 @@ var ChocolateEvents = {
         form.save(true);
     },
     cardSaveEvent: function ($context) {
-//            .on('click', '.card-menu-save', this.cardSaveFromMenuHandler)
         $context
+            .on('click', '.card-menu-save', this.cardSaveFromMenuHandler)
             .on('click', '.card-save', this.cardSaveButtonHandler);
     },
     cardSaveFromMenuHandler: function () {
