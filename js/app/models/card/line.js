1499dad20018ddba5b58db5d306cfc287b397abe
Целищев Семен <tselishev.semen@yandex.ru>
2015-03-02 15:23:03 +0300

    closed #145

--- js/app/models/card/line.js
+++ js/app/models/card/line.js
@@ -15,6 +15,39 @@ var LineCardElement = (function (CardElement) {
              */
             _renderControl: function () {
                 return '';
+            },
+            /**
+             * @override
+             * @returns {string}
+             * @protected
+             */
+            _renderBeginData: function () {
+                return '';
+            },
+            /**
+             * @override
+             * @returns {string}
+             * @protected
+             */
+            _renderEndData: function () {
+                return '';
+            },
+            /**
+             * @override
+             * @returns {Number}
+             * @protected
+             */
+            _getMinHeight: function () {
+                return 20;
+            },
+            /**
+             * @protected
+             * @override
+             * @returns {Function}
+             */
+            _getCallback: function () {
+                return function () {
+                };
             }
         });
 })(CardElement);
