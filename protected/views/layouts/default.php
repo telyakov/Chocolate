c86bd0e0bd207cbd8f4646c3ab813ba5eabda4f5
Целищев Семен <tselishev.semen@yandex.ru>
2015-03-03 15:10:35 +0300

    M: minimize  generated html in php

--- protected/views/layouts/default.php
+++ protected/views/layouts/default.php
@@ -2,33 +2,36 @@
 /* @var $this SiteController */
 ?>
     <!DOCTYPE html>
-    <html lang="ru">
+    <html lang=ru>
     <head>
-        <meta charset="UTF-8"/>
+        <meta charset=UTF-8/>
         <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
-        <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
-        <link href="/css/libs/erp.css" rel="stylesheet">
-        <link href="/css/main.css" rel="stylesheet">
+        <link href=/images/favicon.ico rel="shortcut icon" type="image/x-icon"/>
+        <link href=/css/libs/erp.css rel=stylesheet>
+        <link href=/css/main.css rel=stylesheet>
         <? if (defined('CHOCOLATE_DEBUG')): ?>
-            <script src="/js/erp.js"></script>
+            <script src=/js/erp.js></script>
         <? else: ?>
-            <script src="/js/erp.min.js"></script>
+            <script src=/js/erp.min.js></script>
         <? endif; ?>
-        <script src="/js/main.js"></script>
+        <script src=/js/main.js></script>
         <? echo CHtml::tag('title', [], Yii::app()->name) ?>
     </head>
     <body>
-    <header id="header"></header>
-    <div id="pagewrap">
-        <ul id="gn-menu" class="gn-menu-main"></ul>
-        <div id="content">
+    <header id=header></header>
+    <div id=pagewrap>
+        <ul id=gn-menu class=gn-menu-main></ul>
+        <div id=content>
             <? echo $content; ?>
         </div>
-    <div id="message"></div>
+        <div id=message></div>
     </div>
     </body>
     </html>
 <?
+/**
+ * @var $user WebUser
+ */
 $user = Yii::app()->user;
 $name = $user->fullName;
 $id = $user->id;
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,20 +1,20 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
-<html lang="ru">
+<html lang=ru>
 <head>
-    <meta charset="UTF-8"/>
+    <meta charset=UTF-8/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-    <link rel="stylesheet" type="text/css" href="/css/default.css"/>
+    <link rel=stylesheet href=/css/default.css>
     <link
-        href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css"
-        rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css"
-          rel="stylesheet">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        href=//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css
+        rel=stylesheet>
+    <link href=//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css
+          rel=stylesheet>
+    <script src=//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js></script>
     <script
-        src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+        src=//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js></script>
     <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src=//html5shiv.googlecode.com/svn/trunk/html5.js></script>
     <![endif]-->
     <?php
     Yii::app()->clientScript->scriptMap = [
@@ -24,8 +24,8 @@
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 <body>
-<div id='login-container'>
-    <section class="center">
+<div id=login-container>
+    <section class=center>
         <?php echo $content; ?>
     </section>
 </div>
